c92ede0752d9df461a052050c4ba190d0438bf05
Duszke333 <duszke333@gmail.com>
2026-06-04 19:12:54 +0200

    Document Err

--- include/Err.h
+++ include/Err.h
@@ -18,6 +18,9 @@
 namespace chof {
     using namespace std;
 
+    /**
+     * @brief Exception wrapper for internal simulation and configuration errors.
+     */
     struct Err {
         string msg;
 
@@ -28,13 +31,31 @@ namespace chof {
         };
     };
 
+    /**
+     * @brief Logs an informational message to standard output.
+     * Appends file name and line number automatically.
+     * @param msg The message to display.
+     */
 #define INFO(msg) std::cout << std::endl << __FILE__<< " (" << __LINE__ << "): " << msg << std::endl;
 
+    /**
+     * @brief Logs a warning message to standard error stream.
+     * Appends file name and line number automatically.
+     * @param msg The warning message to display.
+     */
 #define WARN(msg) std::cerr << std::endl << __FILE__<< " (" << __LINE__ << "): warning: " << msg;
 
-
+    /**
+     * @brief Logs an error message and throws an exception.
+     * Halts execution by throwing a chof::Err exception.
+     * @param msg The error message detailing the failure.
+     */
 #define ERROR(msg) { std::cerr << std::endl << std::string(__FILE__) + " ("  + std::to_string(__LINE__) + "): error: " + msg  << std::endl << std::flush; throw chof::Err( std::string(__FILE__) + " ("  + std::to_string(__LINE__) + "): error: " + msg );}
 
-
+    /**
+     * @brief Logs an internal system error and throws an exception.
+     * Used for critical algorithmic failures (e.g., out of bounds, logic flaws).
+     * @param msg The internal error message.
+     */
 #define INTERNAL(msg) { std::cerr << std::endl << std::string(__FILE__) + " ("  + std::to_string(__LINE__) + "): error: " + msg  << std::endl << std::flush;  throw chof::Err(std::string(__FILE__) + " (" + std::to_string(__LINE__) + "): internal error: " + msg); }
 }
