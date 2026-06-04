2b14e779e7c4a4d354d6e819d2472695e37fda68
Duszke333 <duszke333@gmail.com>
2026-06-04 20:11:44 +0200

    Document ClusteringConstructionHeuristic

--- include/ClusteringConstructionHeuristic.h
+++ include/ClusteringConstructionHeuristic.h
@@ -16,64 +16,90 @@
 
 
 namespace chof {
-    // using namespace __gnu_debug;
     using namespace std;
-    // using json = nlohmann::json;
 
     /**
-     * ClusteringHeuristic implements ConstructionHeuristicConcept interface
-    **/
-
+     * @brief An ensemble heuristic that manages a cluster of multiple base heuristics.
+     * * This class evaluates the problem instance using multiple independent Construction
+     * Heuristics (e.g., networks with slightly different weights or structures) and
+     * returns the best generated solution among them.
+     * * @tparam CHType The underlying Construction Heuristic type.
+     */
     template<ConstructionHeuristicConcept CHType>
     class ClusteringConstructionHeuristic {
         friend class boost::serialization::access;
 
     public:
+        /**
+         * @brief Configuration for the clustered ensemble.
+         */
         struct ConfigType {
             using CHT = CHType;
 
-            int num;
-            typename CHType::ConfigType CHConf;
+            int num; ///< Number of heuristic instances in the cluster.
+            typename CHType::ConfigType CHConf; ///< Shared configuration for all child heuristics.
         };
 
-        ConfigType Conf;
-        vector<CHType> CHS;
+        ConfigType Conf; ///< General configuration for the cluster.
+        vector<CHType> CHS; ///< Vector storing the individual heuristic instances.
 
-    public:
         template<class Archive>
         void serialize(Archive &ar, const unsigned int version) {
             ar & CHS;
         }
 
-        // ConstructionHeuristicConcept interface
-        typedef CHType::DataType DataType;
-
-        bool maximize() const {
-            assert(!CHS.empty());
-            return CHS.front().maximize();
-        };
-
         ClusteringConstructionHeuristic() {
         }
 
+        /**
+         * @brief Constructs the ensemble based on the provided configuration.
+         * @param _Conf The configuration specifying the number of heuristics and their setup.
+         */
         ClusteringConstructionHeuristic(const ConfigType &_Conf) : Conf(_Conf), CHS(_Conf.num, CHType(_Conf.CHConf)) {
         }
 
-
+        /**
+         * @brief Constructs the ensemble by cloning a provided heuristic.
+         * @param _M Number of clones to create.
+         * @param CS The base heuristic to copy.
+         */
         ClusteringConstructionHeuristic(int _M, CHType &CS) : CHS(_M, CS) {
         }
 
+        // ConstructionHeuristicConcept interface
+        typedef CHType::DataType DataType;
+
+        /**
+         * @brief Returns the optimization direction (delegated to the first child heuristic).
+         */
+        bool maximize() const {
+            assert(!CHS.empty());
+            return CHS.front().maximize();
+        }
+
+        /**
+         * @brief Calculates the total number of tunable parameters across the entire ensemble.
+         * @return Size of a single heuristic's parameters multiplied by the number of heuristics in the cluster.
+         */
         int getParamsSize() const {
             return CHS.size() * CHS.front().getParamsSize();
         }
 
+        /**
+         * @brief Extracts parameters from all child heuristics into a single flat vector.
+         * @param Params Vector where the parameters will be appended.
+         */
         void getParams(vector<double> &Params) const {
             for (auto &ch: CHS) {
                 ch.getParams(Params);
             }
         }
 
-
+        /**
+         * @brief Distributes a flat array of parameters evenly across all child heuristics.
+         * @param params Pointer to the array of double parameters.
+         * @param n Total size of the array.
+         */
         ClusteringConstructionHeuristic &setParams(const double *params, int n) {
             int ch_size = CHS.front().getParamsSize();
             assert(n == ch_size * CHS.size());
@@ -84,9 +110,15 @@ namespace chof {
             return *this;
         }
 
+        /**
+         * @brief Core ensemble logic: Runs all child heuristics and returns the best solution.
+         * @param Data The problem instance to be solved.
+         * @return The best generated solution.
+         */
         DataType::SolutionType run(const DataType &Data) {
             typename DataType::SolutionType Sol, SolTmp;
 
+            // Initialize with the worst possible objective value
             Sol.setObj(CHS.front().maximize() ? -DBL_MAX : DBL_MAX);
 
             int c = 0;
@@ -94,10 +126,11 @@ namespace chof {
             for (auto &CH: CHS) {
                 SolTmp = CH.run(Data);
 
+                // Check if the current heuristic produced a strictly better result
                 if (CHS.front().maximize() ? SolTmp.getObj() > Sol.getObj() : SolTmp.getObj() < Sol.getObj()) {
                     swap(Sol, SolTmp);
-                    // Sol = SolTmp;
 
+                    // If the solution supports clustering, tag it with the winning heuristic's ID
                     if constexpr (ClusteredSolutionConcept<typename DataType::SolutionType>) {
                         Sol.setCluster(c);
                     }
@@ -110,21 +143,19 @@ namespace chof {
         }
 
         // end of interface
+
+        /**
+         * @brief Access a specific heuristic in the cluster.
+         * @param c Index of the heuristic.
+         */
         CHType &operator[](int c) {
             return CHS[c];
         }
     };
 
-    // template<typename T>
-    // inline void to_json(nlohmann::json& j, const T& c) {
-    //     to_json<typename T::ParentType::ConfigType>(j, c);
-    // }
-    // template<typename CHType>
-    // void to_json(nlohmann::json& j, const typename ClusteringConstructionHeuristic<CHType>::ConfigType& c) {
-    //     j = nlohmann::json{};
-    //     j.emplace("num", c.num);
-    //     j.emplace("CHConf", c.CHConf );
-    // }
+    // ==================
+    // JSON SERIALIZATION
+    // ==================
 
     template<typename T>
     auto to_json(nlohmann::json &j, const T &c)
@@ -133,17 +164,6 @@ namespace chof {
         j.emplace("num", c.num);
         j.emplace("CHConf", c.CHConf);
     }
-
-
-    // template<typename T>
-    // inline void from_json(const nlohmann::json &j, T &c) {
-    //     from_json<typename T::ParentType::ConfigType>(j, c);
-    // }
-    // template<typename CHType>
-    // void from_json(const nlohmann::json &j, typename ClusteringConstructionHeuristic<CHType>::ConfigType& c) {
-    //     j.at("num").get_to(c.num);
-    //     j.at("CHConf").get_to(c.CHConf);
-    // }
 
     template<typename T>
     auto from_json(nlohmann::json &j, T &c)
